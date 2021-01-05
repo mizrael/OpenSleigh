b2b9f1bf42a5aa8551ca0a0fc2525bf6774a3813
David Guida <Davide.Guida@equisoft.com>
2021-01-04 20:45:31 -0500

    added more tests

---
+++ tests/OpenSleigh.Persistence.Mongo.Tests/Unit/OutboxRepositoryTests.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+using MongoDB.Driver;
+using NSubstitute;
+using Xunit;
+
+namespace OpenSleigh.Persistence.Mongo.Tests.Unit
+{
+    public class OutboxRepositoryTests
+    {
+        [Fact]
+        public async Task ctor_should_throw_when_DbContext_null()
+        {
+            var serializer = NSubstitute.Substitute.For<ISerializer>();
+            Assert.Throws<ArgumentNullException>(() => new OutboxRepository(null, serializer));
+        }
+
+        [Fact]
+        public async Task ctor_should_throw_when_Serializer_null()
+        {
+            var dbCtx = NSubstitute.Substitute.For<IDbContext>();
+            Assert.Throws<ArgumentNullException>(() => new OutboxRepository(dbCtx, null));
+        }
+
+        [Fact]
+        public async Task ReadMessagesToProcess_should_return_empty_collection_if_no_data_available()
+        {
+            var dbCtx = NSubstitute.Substitute.For<IDbContext>();
+            var serializer = NSubstitute.Substitute.For<ISerializer>();
+            var sut = new OutboxRepository(dbCtx, serializer);
+
+            var result = await sut.ReadMessagesToProcess();
+            result.Should().NotBeNull().And.BeEmpty();
+        }
+    }
+}
