d04bbf32f8d2e14cb3f1a1999ffcd86cd1beea40
David Guida <Davide.Guida@equisoft.com>
2021-01-08 16:23:38 -0500

    cleanup

--- src/OpenSleigh.Core/Persistence/NullTransaction.cs
+++ src/OpenSleigh.Core/Persistence/NullTransaction.cs
@@ -1,8 +1,10 @@
+using System.Diagnostics.CodeAnalysis;
 using System.Threading;
 using System.Threading.Tasks;
 
 namespace OpenSleigh.Core.Persistence
 {
+    [ExcludeFromCodeCoverage]
     public class NullTransaction : ITransaction
     {
         public Task CommitAsync(CancellationToken cancellationToken = default) => Task.CompletedTask;
