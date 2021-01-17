62a309128e89bd053a725c828e180c472125bba7
David Guida <Davide.Guida@equisoft.com>
2021-01-17 18:59:09 -0500

    initializing State only for messages that can start the Saga

--- src/OpenSleigh.Core/ISagaStateFactory.cs
+++ src/OpenSleigh.Core/ISagaStateFactory.cs
@@ -5,6 +5,7 @@ namespace OpenSleigh.Core
     public interface ISagaStateFactory<out TD>
         where TD : SagaState
     {
+        //TODO: this should probably be a generic method
         TD Create(IMessage message);
     }
 }
