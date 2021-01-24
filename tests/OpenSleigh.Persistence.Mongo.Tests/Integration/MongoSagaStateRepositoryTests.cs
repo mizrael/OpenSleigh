e18a960db87d3748ffe90a3433e9405d2a3a1542
David Guida <Davide.Guida@equisoft.com>
2021-01-23 21:58:37 -0500

    refactoring

--- tests/OpenSleigh.Persistence.Mongo.Tests/Integration/MongoSagaStateRepositoryTests.cs
+++ tests/OpenSleigh.Persistence.Mongo.Tests/Integration/MongoSagaStateRepositoryTests.cs
@@ -21,13 +21,20 @@ namespace OpenSleigh.Persistence.Mongo.Tests.Integration
             _fixture = fixture;
         }
 
-        [Fact]
-        public async Task LockAsync_should_create_and_return_locked_item_if_not_existing()
+        private MongoSagaStateRepository CreateSut(MongoSagaStateRepositoryOptions options = null)
         {
             var serializer = new JsonSerializer();
 
-            var options = new MongoSagaStateRepositoryOptions(TimeSpan.FromMinutes(1));
+            options ??= MongoSagaStateRepositoryOptions.Default;
+
             var sut = new MongoSagaStateRepository(_fixture.DbContext, serializer, options);
+            return sut;
+        }
+
+        [Fact]
+        public async Task LockAsync_should_create_and_return_locked_item_if_not_existing()
+        {
+            var sut = CreateSut();
 
             var newState = DummyState.New();
 
@@ -37,31 +44,42 @@ namespace OpenSleigh.Persistence.Mongo.Tests.Integration
             state.Bar.Should().Be(newState.Bar);
             state.Foo.Should().Be(newState.Foo);
         }
-        
+
         [Fact]
         public async Task LockAsync_should_throw_if_item_locked()
         {
-            var serializer = new JsonSerializer();
-
-            var options = new MongoSagaStateRepositoryOptions(TimeSpan.FromMinutes(1));
-            var sut = new MongoSagaStateRepository(_fixture.DbContext, serializer, options);
+            var sut = CreateSut();
 
             var newState = DummyState.New();
 
-            var (state, lockId) = await sut.LockAsync(newState.Id, newState, CancellationToken.None);
+            await sut.LockAsync(newState.Id, newState, CancellationToken.None);
 
             var ex = await Assert.ThrowsAsync<LockException>(async () => await sut.LockAsync(newState.Id, newState, CancellationToken.None));
-            ex.Message.Should().Contain($"saga state '{state.Id}' is already locked");
+            ex.Message.Should().Contain($"saga state '{newState.Id}' is already locked");
+        }
+
+        [Fact]
+        public async Task LockAsync_should_return_state_if_lock_expired()
+        {
+            var options = new MongoSagaStateRepositoryOptions(TimeSpan.Zero);
+            var sut = CreateSut(options);
+
+            var newState = DummyState.New();
+
+            var (firstState, firstLockId) = await sut.LockAsync(newState.Id, newState, CancellationToken.None);
+
+            await Task.Delay(500);
+            
+            var (secondState, secondLockId) = await sut.LockAsync(newState.Id, newState, CancellationToken.None);
+            secondLockId.Should().NotBe(firstLockId);
+            secondState.Should().NotBeNull();
         }
 
         [Fact]
         public async Task LockAsync_should_allow_different_saga_state_types_to_share_the_correlation_id()
         {
-            var serializer = new JsonSerializer();
+            var sut = CreateSut();
 
-            var options = new MongoSagaStateRepositoryOptions(TimeSpan.FromMinutes(1));
-            var sut = new MongoSagaStateRepository(_fixture.DbContext, serializer, options);
-            
             var correlationId = Guid.NewGuid();
 
             var newState = new DummyState(correlationId, "lorem", 42);
