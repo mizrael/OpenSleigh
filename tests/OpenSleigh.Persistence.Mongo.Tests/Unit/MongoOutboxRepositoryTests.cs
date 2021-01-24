a9fcc4b01172997bbee74d6c18d86a82cad080ed
David Guida <Davide.Guida@equisoft.com>
2021-01-24 18:28:38 -0500

    added more tests

--- tests/OpenSleigh.Persistence.Mongo.Tests/Unit/MongoOutboxRepositoryTests.cs
+++ tests/OpenSleigh.Persistence.Mongo.Tests/Unit/MongoOutboxRepositoryTests.cs
@@ -33,12 +33,42 @@ namespace OpenSleigh.Persistence.Mongo.Tests.Unit
         [Fact]
         public async Task ReadMessagesToProcess_should_return_empty_collection_if_no_data_available()
         {
-            var dbCtx = NSubstitute.Substitute.For<IDbContext>();
-            var serializer = NSubstitute.Substitute.For<ISerializer>();
-            var sut = new MongoOutboxRepository(dbCtx, serializer, MongoOutboxRepositoryOptions.Default);
+            var sut = CreateSut();
 
             var result = await sut.ReadMessagesToProcess();
             result.Should().NotBeNull().And.BeEmpty();
+        }
+
+        [Fact]
+        public async Task ReleaseAsync_should_throw_if_message_null()
+        {
+            var sut = CreateSut();
+
+            await Assert.ThrowsAsync<ArgumentNullException>(async () => await sut.ReleaseAsync(null, Guid.Empty));
+        }
+
+        [Fact]
+        public async Task AppendAsync_should_throw_if_message_null()
+        {
+            var sut = CreateSut();
+
+            await Assert.ThrowsAsync<ArgumentNullException>(async () => await sut.AppendAsync(null));
+        }
+
+        [Fact]
+        public async Task LockAsync_should_throw_if_message_null()
+        {
+            var sut = CreateSut();
+
+            await Assert.ThrowsAsync<ArgumentNullException>(async () => await sut.LockAsync(null));
+        }
+
+        private static MongoOutboxRepository CreateSut()
+        {
+            var dbCtx = NSubstitute.Substitute.For<IDbContext>();
+            var serializer = NSubstitute.Substitute.For<ISerializer>();
+            var sut = new MongoOutboxRepository(dbCtx, serializer, MongoOutboxRepositoryOptions.Default);
+            return sut;
         }
     }
 }
