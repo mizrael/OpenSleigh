0251b01868fec6a8bf85e262cd35c34f8832cc6a
David Guida <Davide.Guida@equisoft.com>
2021-01-25 09:13:00 -0500

    added E2E tests

---
+++ tests/OpenSleigh.Persistence.SQL.Tests/E2E/SqlEventBroadcastingScenario.cs
@@ -0,0 +1,29 @@
+﻿using OpenSleigh.Core.DependencyInjection;
+using OpenSleigh.Core.Tests.E2E;
+using OpenSleigh.Persistence.InMemory;
+using OpenSleigh.Persistence.SQL.Tests.Fixtures;
+using Xunit;
+
+namespace OpenSleigh.Persistence.SQL.Tests.E2E
+{
+    public class SqlEventBroadcastingScenario : EventBroadcastingScenario, IClassFixture<DbFixture>
+    {
+        private readonly DbFixture _fixture;
+
+        public SqlEventBroadcastingScenario(DbFixture fixture)
+        {
+            _fixture = fixture;
+        }
+        
+        protected override void ConfigureTransportAndPersistence(IBusConfigurator cfg)
+        {
+            var sqlCfg = new SqlConfiguration(_fixture.ConnectionString);
+
+            cfg.UseInMemoryTransport()
+                .UseSqlPersistence(sqlCfg);
+        }
+
+        protected override void ConfigureSagaTransport<TS, TD>(ISagaConfigurator<TS, TD> cfg) =>
+            cfg.UseInMemoryTransport();
+    }
+}
---
+++ tests/OpenSleigh.Persistence.SQL.Tests/E2E/SqlSimpleSagaScenario.cs
@@ -0,0 +1,29 @@
+﻿using OpenSleigh.Core.DependencyInjection;
+using OpenSleigh.Core.Tests.E2E;
+using OpenSleigh.Persistence.InMemory;
+using OpenSleigh.Persistence.SQL.Tests.Fixtures;
+using Xunit;
+
+namespace OpenSleigh.Persistence.SQL.Tests.E2E
+{
+    public class SqlSimpleSagaScenario : SimpleSagaScenario, IClassFixture<DbFixture>
+    {
+        private readonly DbFixture _fixture;
+
+        public SqlSimpleSagaScenario(DbFixture fixture)
+        {
+            _fixture = fixture;
+        }
+    
+        protected override void ConfigureTransportAndPersistence(IBusConfigurator cfg)
+        {
+            var sqlCfg = new SqlConfiguration(_fixture.ConnectionString);
+
+            cfg.UseInMemoryTransport()
+                .UseSqlPersistence(sqlCfg);
+        }
+
+        protected override void ConfigureSagaTransport<TS, TD>(ISagaConfigurator<TS, TD> cfg) => 
+            cfg.UseInMemoryTransport();
+    }
+}
