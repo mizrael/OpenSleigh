d9cf571d51d71db56a72308fa2fdb0b30dd77f2a
David Guida <david.guida.03@gmail.com>
2021-01-26 10:09:05 -0500

    added logging

--- src/OpenSleigh.Persistence.InMemory/Messaging/InMemoryPublisher.cs
+++ src/OpenSleigh.Persistence.InMemory/Messaging/InMemoryPublisher.cs
@@ -2,6 +2,7 @@
 using System.Runtime.CompilerServices;
 using System.Threading;
 using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
 using OpenSleigh.Core.Messaging;
 
 [assembly: InternalsVisibleTo("OpenSleigh.Persistence.InMemory.Tests")]
@@ -10,10 +11,12 @@ namespace OpenSleigh.Persistence.InMemory.Messaging
     internal class InMemoryPublisher : IPublisher 
     {
         private readonly IChannelFactory _channelFactory;
+        private readonly ILogger<InMemoryPublisher> _logger;
 
-        public InMemoryPublisher(IChannelFactory channelFactory)
+        public InMemoryPublisher(IChannelFactory channelFactory, ILogger<InMemoryPublisher> logger)
         {
             _channelFactory = channelFactory ?? throw new ArgumentNullException(nameof(channelFactory));
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
         }
         
         public Task PublishAsync(IMessage message, CancellationToken cancellationToken = default)
@@ -28,9 +31,17 @@ namespace OpenSleigh.Persistence.InMemory.Messaging
             where TM : IMessage
         {
             var writer = _channelFactory.GetWriter<TM>();
-            if(writer is not null)
+            if (writer is not null)
+            {
+                _logger.LogInformation($"publishing message '{message.Id}'...");
                 await writer.WriteAsync(message, cancellationToken)
                     .ConfigureAwait(false);
+            }
+            else
+            {
+                _logger.LogWarning($"no suitable publisher found for message '{message.Id}' with type '{typeof(TM).FullName}' !");
+            }
+                
         }
     }
 }
