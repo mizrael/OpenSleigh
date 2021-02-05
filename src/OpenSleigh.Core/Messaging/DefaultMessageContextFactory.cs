a9af553953af1acbc67d325ba8182a9a474cf991
David Guida <david.guida.03@gmail.com>
2021-02-04 21:01:03 -0500

    added retry policies

--- src/OpenSleigh.Core/Messaging/DefaultMessageContextFactory.cs
+++ src/OpenSleigh.Core/Messaging/DefaultMessageContextFactory.cs
@@ -1,5 +1,8 @@
-﻿namespace OpenSleigh.Core.Messaging
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace OpenSleigh.Core.Messaging
 {
+    [ExcludeFromCodeCoverage] //only if the implementation doesn't get more complex
     internal class DefaultMessageContextFactory : IMessageContextFactory
     {
         public IMessageContext<TM> Create<TM>(TM message) where TM : IMessage
--- src/OpenSleigh.Core/SagaRunner.cs
+++ src/OpenSleigh.Core/SagaRunner.cs
@@ -17,33 +17,25 @@ namespace OpenSleigh.Core
         private readonly ISagaFactory<TS, TD> _sagaFactory;
         private readonly ITransactionManager _transactionManager;
         private readonly ILogger<SagaRunner<TS, TD>> _logger;
-        
+        private readonly ISagaPolicyFactory<TS, TD> _policyFactory;
+
         public SagaRunner(ISagaFactory<TS, TD> sagaFactory,
                           ISagaStateService<TS, TD> sagaStateService, 
                           ITransactionManager transactionManager,
+                          ISagaPolicyFactory<TS, TD> policyFactory,
                           ILogger<SagaRunner<TS, TD>> logger)
         {
             _sagaFactory = sagaFactory ?? throw new ArgumentNullException(nameof(sagaFactory));
             _sagaStateService = sagaStateService ?? throw new ArgumentNullException(nameof(sagaStateService));
             _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+            _policyFactory = policyFactory ?? throw new ArgumentNullException(nameof(policyFactory));
             _transactionManager = transactionManager ?? throw new ArgumentNullException(nameof(transactionManager));
         }
 
-        public async Task RunAsync<TM>(IMessageContext<TM> messageContext, CancellationToken cancellationToken)
+        public async Task RunAsync<TM>(IMessageContext<TM> messageContext, CancellationToken cancellationToken = default)
             where TM : IMessage
         {
-            var policy = Policy.DelayedRetry(builder =>
-            {
-                builder.WithDelay(i => TimeSpan.FromSeconds(i))
-                    .Handle<LockException>()
-                    .WithMaxRetries(10)
-                    .OnException(ctx =>
-                    {
-                        _logger.LogWarning(
-                            $"unable to lock state for saga '{messageContext.Message.CorrelationId}': '{ctx.Exception.Message}'. Retrying...");
-                    });
-            });
-            var (state, lockId) = await policy.WrapAsync(() => _sagaStateService.GetAsync(messageContext, cancellationToken));
+            var (state, lockId) = await GetStateAsync(messageContext, cancellationToken);
 
             if (state.IsCompleted())
             {
@@ -57,18 +49,21 @@ namespace OpenSleigh.Core
                 return;
             }
 
+            var saga = _sagaFactory.Create(state);
+            if (null == saga)
+                throw new SagaException($"unable to create Saga of type '{typeof(TS).FullName}'");
+
+            if (saga is not IHandleMessage<TM> handler)
+                throw new ConsumerNotFoundException(typeof(TM));
+            
             var transaction = await _transactionManager.StartTransactionAsync(cancellationToken);
             try
             {
-                var saga = _sagaFactory.Create(state);
-                if (null == saga)
-                    throw new SagaException($"unable to create Saga of type '{typeof(TS).FullName}'");
-
-                if (saga is not IHandleMessage<TM> handler)
-                    throw new ConsumerNotFoundException(typeof(TM));
-
-                //TODO: add configurable retry policy
-                await handler.HandleAsync(messageContext, cancellationToken);
+                var policy = _policyFactory.Create<TM>();
+                if (policy is null)
+                    await handler.HandleAsync(messageContext, cancellationToken);
+                else 
+                    await policy.WrapAsync(() => handler.HandleAsync(messageContext, cancellationToken));
 
                 state.SetAsProcessed(messageContext.Message);
 
@@ -81,6 +76,23 @@ namespace OpenSleigh.Core
                 await transaction.RollbackAsync(cancellationToken);
                 throw;
             }
+        }
+
+        private async Task<(TD state, Guid lockId)> GetStateAsync<TM>(IMessageContext<TM> messageContext, CancellationToken cancellationToken)
+            where TM : IMessage
+        {
+            var policy = Policy.Retry(builder =>
+            {
+                builder.WithDelay(i => TimeSpan.FromSeconds(i))
+                    .Handle<LockException>()
+                    .WithMaxRetries(10)
+                    .OnException(ctx =>
+                    {
+                        _logger.LogWarning(
+                            $"unable to lock state for saga '{messageContext.Message.CorrelationId}': '{ctx.Exception.Message}'. Retrying...");
+                    });
+            });
+            return await policy.WrapAsync(() => _sagaStateService.GetAsync(messageContext, cancellationToken));
         }
     }
 }
