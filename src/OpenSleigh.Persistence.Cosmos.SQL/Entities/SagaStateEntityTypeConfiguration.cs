55e623bb2de652a251749964c88980694c8c5a77
David Guida <david.guida.03@gmail.com>
2021-03-23 10:34:21 -0400

    moved classes

---
+++ src/OpenSleigh.Persistence.Cosmos.SQL/Entities/SagaStateEntityTypeConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace OpenSleigh.Persistence.Cosmos.SQL.Entities
+{
+    [ExcludeFromCodeCoverage]
+    internal class SagaStateEntityTypeConfiguration : IEntityTypeConfiguration<SagaState>
+    {
+        public void Configure(EntityTypeBuilder<SagaState> builder)
+        {
+            builder.ToContainer("SagaStates")
+                            .HasNoDiscriminator();
+
+            builder.HasKey(e => new {e.CorrelationId, e.Type});
+            builder.HasPartitionKey(e => e.PartitionKey);
+        }
+    }
+
+}
