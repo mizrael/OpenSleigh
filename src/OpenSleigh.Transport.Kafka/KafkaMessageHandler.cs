c0d0e558b4ee2a0fdf766df1bb59abb39fc903e0
David Guida <david.guida.03@gmail.com>
2021-03-24 11:35:45 -0400

    refactoring

--- src/OpenSleigh.Transport.Kafka/KafkaMessageHandler.cs
+++ src/OpenSleigh.Transport.Kafka/KafkaMessageHandler.cs
@@ -28,51 +28,62 @@ namespace OpenSleigh.Transport.Kafka
 
         public async Task HandleAsync(ConsumeResult<Guid, byte[]> result, QueueReferences queueReferences, CancellationToken cancellationToken = default)
         {
-            IMessage message = null;
+            var message = Parse(result, queueReferences);
+            if (message is not null)
+                await Process(message, queueReferences, cancellationToken);
+        }
 
+        private IMessage Parse(ConsumeResult<Guid, byte[]> result, QueueReferences queueReferences)
+        {
             try
             {
-                message = _messageParser.Parse(result);
-
-                _logger.LogDebug("received message '{MessageId}' from Topic '{Topic}'. Processing...",
-                                 message.Id, queueReferences.TopicName);
+                return _messageParser.Parse(result);
             }
             catch (Exception ex)
             {
                 _logger.LogError(ex, "an exception has occurred while consuming a message: {Exception}", ex.Message);
+                return null;
             }
-
-            if (message is null)
-                return;
-
+        }
+        
+        private async Task Process(IMessage message, QueueReferences queueReferences, CancellationToken cancellationToken)
+        {
+            _logger.LogDebug("received message '{MessageId}' from Topic '{Topic}'. Processing...", 
+                             message.Id, queueReferences.TopicName);
             try
             {
                 await _messageProcessor.ProcessAsync((dynamic)message, cancellationToken);
             }
             catch (Exception ex)
             {
-                await PushToDeadLetter(queueReferences, message, ex, cancellationToken);
+                await HandleProcessErrors(message, queueReferences, ex, cancellationToken);
             }
         }
 
-        private async Task PushToDeadLetter(QueueReferences queueReferences, IMessage message, Exception ex, CancellationToken cancellationToken)
+        private async Task HandleProcessErrors(IMessage message, QueueReferences queueReferences, Exception ex,
+                                                CancellationToken cancellationToken)
         {
-            _logger.LogWarning(ex,
-                "an exception has occurred while consuming message '{MessageId}': {Exception}",
-                message.Id, ex.Message);
+            _logger.LogWarning(ex, "an exception has occurred while consuming message '{MessageId}': {Exception}",
+                               message.Id, ex.Message);
+
+            if (string.IsNullOrWhiteSpace(queueReferences.DeadLetterTopicName))
+                return;
+
             try
             {
-                if (!string.IsNullOrWhiteSpace(queueReferences.DeadLetterTopicName))
-                    await _publisher.PublishAsync(message, queueReferences.DeadLetterTopicName,
-                        additionalHeaders: new[]
-                        {
-                            new Header(HeaderNames.Error, Encoding.UTF8.GetBytes(ex.Message))
-                        },
-                        cancellationToken: cancellationToken);
+                _logger.LogWarning("pushing message '{MessageId}' to DLQ '{DeadLetterQueue}' ...",
+                                    message.Id, queueReferences.DeadLetterTopicName);
+
+                await _publisher.PublishAsync(message, queueReferences.DeadLetterTopicName,
+                    additionalHeaders: new[]
+                    {
+                        new Header(HeaderNames.Error, Encoding.UTF8.GetBytes(ex.Message))
+                    },
+                    cancellationToken: cancellationToken);
             }
             catch (Exception dlqEx)
             {
-                _logger.LogWarning(ex, "an exception has occurred while publishing message '{MessageId}' to DLQ '{DeadLetterQueue}': {Exception}",
+                _logger.LogWarning(dlqEx, "an exception has occurred while publishing message '{MessageId}' to DLQ '{DeadLetterQueue}': {Exception}",
                     message.Id,
                     queueReferences.DeadLetterTopicName,
                     dlqEx.Message);
