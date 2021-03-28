0765f0f36520eeae21cfcea9a0be8497a2b1e655
David Guida <david.guida.03@gmail.com>
2021-03-28 13:36:19 -0400

    added notifications service

--- samples/Sample4/OpenSleigh.Samples.Sample4.Common/Messages.cs
+++ samples/Sample4/OpenSleigh.Samples.Sample4.Common/Messages.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Text.Json.Serialization;
 using OpenSleigh.Core.Messaging;
 
 namespace OpenSleigh.Samples.Sample4.Common
@@ -16,39 +15,15 @@ namespace OpenSleigh.Samples.Sample4.Common
         public static ProcessCreditCheck New(Guid orderId) => new (Guid.NewGuid(), orderId);
     }
 
-    public record CrediCheckCompleted(Guid Id, Guid OrderId) : IEvent
-    {
-        public Guid CorrelationId => this.OrderId;
-        public static CrediCheckCompleted New(Guid orderId) => new (Guid.NewGuid(), orderId);
-    }
-
     public record CheckInventory(Guid Id, Guid OrderId) : ICommand
     {
         public Guid CorrelationId => this.OrderId;
         public static CheckInventory New(Guid orderId) => new(Guid.NewGuid(), orderId);
     }
 
-    public record InventoryCheckCompleted(Guid Id, Guid OrderId) : IEvent
-    {
-        public Guid CorrelationId => this.OrderId;
-        public static InventoryCheckCompleted New(Guid orderId) => new(Guid.NewGuid(), orderId);
-    }
-
     public record ProcessShipping(Guid Id, Guid OrderId) : ICommand
     {
         public Guid CorrelationId => this.OrderId;
         public static ProcessShipping New(Guid orderId) => new(Guid.NewGuid(), orderId);
-    }
-
-    public record ShippingCompleted(Guid Id, Guid OrderId) : IEvent
-    {
-        public Guid CorrelationId => this.OrderId;
-        public static ShippingCompleted New(Guid orderId) => new(Guid.NewGuid(), orderId);
-    }
-
-    public record OrderSagaCompleted(Guid Id, Guid OrderId) : IEvent
-    {
-        public Guid CorrelationId => this.OrderId;
-        public static OrderSagaCompleted New(Guid orderId) => new(Guid.NewGuid(), orderId);
     }
 }
---
+++ samples/Sample4/OpenSleigh.Samples.Sample4.NotificationsService/Program.cs
@@ -0,0 +1,51 @@
+﻿using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using OpenSleigh.Core.DependencyInjection;
+using OpenSleigh.Persistence.SQL;
+using OpenSleigh.Samples.Sample4.Common;
+using OpenSleigh.Samples.Sample4.NotificationsService.Sagas;
+using OpenSleigh.Transport.RabbitMQ;
+
+namespace OpenSleigh.Samples.Sample4.NotificationsService
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var hostBuilder = CreateHostBuilder(args);
+            var host = hostBuilder.Build();
+
+            await host.RunAsync();
+        }
+
+        static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+            .ConfigureServices((hostContext, services) =>
+            {
+                services.AddLogging(cfg =>
+                    {
+                        cfg.AddConsole();
+                    })
+                    .AddOpenSleigh(cfg =>
+                    {
+                        var rabbitSection = hostContext.Configuration.GetSection("Rabbit");
+                        var rabbitCfg = new RabbitConfiguration(rabbitSection["HostName"], 
+                            rabbitSection["UserName"],
+                            rabbitSection["Password"]);
+
+                        var sqlConnStr = hostContext.Configuration.GetConnectionString("sql");
+                        var sqlConfig = new SqlConfiguration(sqlConnStr);
+
+                        cfg.UseRabbitMQTransport(rabbitCfg)
+                            .UseSqlPersistence(sqlConfig);
+
+                        cfg.AddSaga<NotificationsSaga, InventoryCheckSagaState>()
+                            .UseStateFactory<CheckInventory>(msg => new InventoryCheckSagaState(msg.CorrelationId))
+                            .UseRabbitMQTransport();
+                    });
+            });
+    }
+}
