271e14ca6a23ea51fa84c9031a637bae6ea0058c
David Guida <david.guida.03@gmail.com>
2021-04-28 17:27:57 -0400

    minor fix

--- src/OpenSleigh.Core/TypeResolver.cs
+++ src/OpenSleigh.Core/TypeResolver.cs
@@ -22,7 +22,11 @@ namespace OpenSleigh.Core
         {
             Type dataType = null;
             foreach (var assembly in _assemblies)
+            {
                 dataType = assembly.GetType(typeName, throwOnError: false, ignoreCase: true);
+                if (dataType is not null)
+                    break;
+            }                
 
             if (dataType is null)
                 _typesByName.TryGetValue(typeName.ToLower(), out dataType);
