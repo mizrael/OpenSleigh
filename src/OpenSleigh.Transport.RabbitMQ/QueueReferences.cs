a3e0af32ff96495f1e2d7bfad1be6b0f2afa9ba6
David Guida <david.guida.03@gmail.com>
2021-04-28 20:49:51 -0400

    added extra ctor for retrocompatibility

--- src/OpenSleigh.Transport.RabbitMQ/QueueReferences.cs
+++ src/OpenSleigh.Transport.RabbitMQ/QueueReferences.cs
@@ -1,4 +1,10 @@
 ﻿namespace OpenSleigh.Transport.RabbitMQ
 {
-    public record QueueReferences(string ExchangeName, string QueueName, string RoutingKey, string DeadLetterExchangeName, string DeadLetterQueue);
+    public record QueueReferences(string ExchangeName, string QueueName, string RoutingKey,
+                                    string DeadLetterExchangeName, string DeadLetterQueue)
+    {
+        public QueueReferences(string ExchangeName, string QueueName,
+                                string DeadLetterExchangeName, string DeadLetterQueue) : this(ExchangeName, QueueName: QueueName, RoutingKey: QueueName,
+                                                                                              DeadLetterExchangeName, DeadLetterQueue){}
+    }
 }
