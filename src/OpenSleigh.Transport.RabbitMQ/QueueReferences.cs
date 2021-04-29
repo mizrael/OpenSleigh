4fc0770a091e054c37d11ec5cf23b83c8f297759
David Guida <david.guida.03@gmail.com>
2021-04-28 20:52:12 -0400

    chores

--- src/OpenSleigh.Transport.RabbitMQ/QueueReferences.cs
+++ src/OpenSleigh.Transport.RabbitMQ/QueueReferences.cs
@@ -3,6 +3,11 @@
     public record QueueReferences(string ExchangeName, string QueueName, string RoutingKey,
                                     string DeadLetterExchangeName, string DeadLetterQueue)
     {
+        /// <summary>
+        /// helper cTor, useful when creating standard messages (eg. commands). It's supposing that a single
+        /// queue is bound to the exchange, hence no real need to use a custom routing key. The queue name will be used to
+        /// route the message, just in case other queues get added to the same exchange
+        /// </summary>
         public QueueReferences(string ExchangeName, string QueueName,
                                 string DeadLetterExchangeName, string DeadLetterQueue) : this(ExchangeName, QueueName: QueueName, RoutingKey: QueueName,
                                                                                               DeadLetterExchangeName, DeadLetterQueue){}
