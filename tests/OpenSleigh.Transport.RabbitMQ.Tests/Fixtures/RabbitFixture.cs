9c6e57874e0ea97309b1d91f9609b9172dbce047
David Guida <davidguida@microsoft.com>
2021-11-10 22:01:19 +0000

    added publisher integration test

--- tests/OpenSleigh.Transport.RabbitMQ.Tests/Fixtures/RabbitFixture.cs
+++ tests/OpenSleigh.Transport.RabbitMQ.Tests/Fixtures/RabbitFixture.cs
@@ -1,9 +1,17 @@
 ﻿using Microsoft.Extensions.Configuration;
+using NSubstitute;
+using RabbitMQ.Client;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
 
 namespace OpenSleigh.Transport.RabbitMQ.Tests.Fixtures
 {
-    public class RabbitFixture
+    public class RabbitFixture : IAsyncLifetime
     {
+        private readonly List<string> _queues = new();
+
         public RabbitFixture()
         {
             var configuration = new ConfigurationBuilder()
@@ -15,6 +23,66 @@ namespace OpenSleigh.Transport.RabbitMQ.Tests.Fixtures
             this.RabbitConfiguration = new RabbitConfiguration(rabbitSection["HostName"],
                 rabbitSection["UserName"],
                 rabbitSection["Password"]);
+        }
+
+        /// <summary>
+        /// returns a RabbitMQ connection. Needs to be disposed after use.
+        /// </summary>
+        public IConnection Connect()
+        {
+            var connectionFactory = new ConnectionFactory()
+            {
+                HostName = RabbitConfiguration.HostName,
+                UserName = RabbitConfiguration.UserName,
+                Password = RabbitConfiguration.Password,
+                Port = AmqpTcpEndpoint.UseDefaultPort,
+                DispatchConsumersAsync = true
+            };
+            return connectionFactory.CreateConnection();
+        }
+
+        public PublisherChannelContext CreatePublisherContext(IModel channel)
+        {
+            var id = System.Guid.NewGuid().ToString();
+            _queues.Add(id);
+
+            var pool = Substitute.For<IPublisherChannelContextPool>();
+            var queueRef = new QueueReferences(id, id, $"{id}.dead", $"{id}.dead");
+
+            channel.ExchangeDeclare(queueRef.ExchangeName, ExchangeType.Topic, false, true);
+            channel.QueueDeclare(queue: queueRef.QueueName,
+                durable: false,
+                exclusive: false,
+                autoDelete: true,
+                arguments: null);
+            channel.QueueBind(queueRef.QueueName,
+                              queueRef.ExchangeName,
+                              routingKey: queueRef.RoutingKey,
+                              arguments: null);
+
+            return new PublisherChannelContext(channel, queueRef, pool);
+        }
+
+        public Task InitializeAsync() => Task.CompletedTask;
+
+        public async Task DisposeAsync()
+        {
+            if (!_queues.Any())
+                return;
+
+            using var connection = Connect();
+            using var channel = connection.CreateModel();
+            
+            foreach (var queueName in _queues) {
+                channel.ExchangeDelete(queueName);
+                channel.QueueDelete(queueName);
+
+                channel.ExchangeDelete(queueName + ".dead");
+                channel.QueueDelete(queueName + ".dead");
+
+                channel.ExchangeDelete(queueName + ".retry");
+                channel.QueueDelete(queueName + ".retry");
+            }
         }
 
         public RabbitConfiguration RabbitConfiguration { get; init; }
---
+++ tests/OpenSleigh.Transport.RabbitMQ.Tests/Integration/RabbitPublisherTests.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.Extensions.Logging;
+using OpenSleigh.Core.Utils;
+using System.ComponentModel;
+using System.Threading.Tasks;
+using System.Text;
+using Xunit;
+using NSubstitute;
+using OpenSleigh.Transport.RabbitMQ.Tests.Fixtures;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using FluentAssertions;
+using System.Threading;
+using System;
+
+namespace OpenSleigh.Transport.RabbitMQ.Tests.Integration
+{
+    [Category("Integration")]
+    [Trait("Category", "Integration")]
+    public class RabbitPublisherTests : IClassFixture<RabbitFixture>
+    {
+        private readonly RabbitFixture _fixture;
+
+        public RabbitPublisherTests(RabbitFixture fixture)
+        {
+            _fixture = fixture;
+        }
+
+        [Fact]
+        public async Task PublishAsync_should_publish_message()
+        {
+            var message = DummyMessage.New();
+            var encodedMessage = Encoding.UTF8.GetBytes(Newtonsoft.Json.JsonConvert.SerializeObject(message));
+
+            var tokenSource = new CancellationTokenSource(TimeSpan.FromSeconds(10));
+
+            using var connection = _fixture.Connect();
+            using var channel = connection.CreateModel();
+
+            var channelContext = _fixture.CreatePublisherContext(channel);
+            bool received = false;
+            var consumer = new AsyncEventingBasicConsumer(channel);
+            consumer.Received += async (_, evt) =>
+            {
+                evt.Body.Should().NotBeNull();
+                evt.Body.ToArray().Should().BeEquivalentTo(encodedMessage);
+                received = true;
+
+                tokenSource.Cancel();
+            };
+            channel.BasicConsume(queue: channelContext.QueueReferences.QueueName, autoAck: false, consumer: consumer);
+
+            var encoder = Substitute.For<ITransportSerializer>();
+            encoder.SerializeAsync(message)
+                .Returns(encodedMessage);
+
+            var logger = Substitute.For<ILogger<RabbitPublisher>>();
+
+            var publisherChannelFactory = NSubstitute.Substitute.For<IPublisherChannelFactory>();
+            publisherChannelFactory.Create(message)
+                .Returns(channelContext);
+
+            var sut = new RabbitPublisher(encoder, logger, publisherChannelFactory);
+            await sut.PublishAsync(message);
+
+            while (!tokenSource.IsCancellationRequested)
+                await Task.Delay(10);
+
+            received.Should().BeTrue(); 
+        }
+    }
+}
