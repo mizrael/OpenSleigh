c7c106f45e88c293428de3aa1c135ff5bee5f92a
David Guida <davidguida@microsoft.com>
2022-01-05 20:56:34 +0000

    added support for Kafka stateless message handlers

---
+++ src/OpenSleigh.Transport.Kafka/KafkaMessageConfiguratorExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using OpenSleigh.Core.DependencyInjection;
+using OpenSleigh.Core.Messaging;
+
+namespace OpenSleigh.Transport.Kafka
+{
+    [ExcludeFromCodeCoverage]
+    public static class KafkaMessageConfiguratorExtensions
+    {
+        public static IMessageHandlerConfigurator<TM> UseKafkaTransport<TM>(this IMessageHandlerConfigurator<TM> configurator)
+            where TM : IMessage
+        {
+            var messageType = typeof(TM);
+            configurator.Services.AddBusSubscriber(
+                typeof(KafkaSubscriber<>).MakeGenericType(messageType));
+            return configurator;
+        }
+    }
+}
